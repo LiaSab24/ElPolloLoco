85766260de37c70446a90f227a81e51fd41350fe
LiaSab24 <164032382+LiaSab24@users.noreply.github.com>
2025-04-11 12:41:41 +0000

    update game.js

--- js/game.js
+++ js/game.js
@@ -2,7 +2,7 @@ let canvas;
 let character = new Image();
 
 function init() {
-    canvas = document.getElementById("gameCanvas");
+    canvas = document.getElementById('canvas');
     canvas.width = 720;
     canvas.height = 480;
 //    const ctx = canvas.getContext("2d");

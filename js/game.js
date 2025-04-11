7f68acdf49e4567515185394a003d86d1c23b683
LiaSab24 <164032382+LiaSab24@users.noreply.github.com>
2025-04-11 12:40:22 +0000

    create game.js file

---
+++ js/game.js
@@ -0,0 +1,10 @@
+let canvas;
+let character = new Image();
+
+function init() {
+    canvas = document.getElementById("gameCanvas");
+    canvas.width = 720;
+    canvas.height = 480;
+//    const ctx = canvas.getContext("2d");
+
+}

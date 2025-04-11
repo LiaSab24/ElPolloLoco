d4e155701a4d7ac0b0d1535ae6cda11593b22e20
LiaSab24 <164032382+LiaSab24@users.noreply.github.com>
2025-04-11 14:05:44 +0000

    update something

--- js/game.js
+++ js/game.js
@@ -1,11 +1,15 @@
 let canvas;
+let ctx;
 let character = new Image();
 
 function init() {
     canvas = document.getElementById('canvas');
     canvas.width = 720;
     canvas.height = 480;
+    ctx = canvas.getContext("2d");
+
     character. src = '../img/pepe/caminata/1.png';
-//    const ctx = canvas.getContext("2d");
+    
+    ctx.drawImage(character, 100, 200, 150, 50);
 
 }

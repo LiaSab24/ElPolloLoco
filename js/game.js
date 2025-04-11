b89788576375a1d3c4dd26073a9dde01484c18e2
LiaSab24 <164032382+LiaSab24@users.noreply.github.com>
2025-04-11 14:46:20 +0000

    changed y-value

--- js/game.js
+++ js/game.js
@@ -11,7 +11,7 @@ function init() {
     character.src = 'img/pepe/caminata/1.png';
   
     character.onload = function () {
-      ctx.drawImage(character, 100, 400, 50, 170);
+      ctx.drawImage(character, 100, 100, 50, 170);
     };
   }
   

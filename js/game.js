fdb6f5c6a1e348af8a5da705728e62d55dbefbf2
LiaSab24 <164032382+LiaSab24@users.noreply.github.com>
2025-04-11 12:49:44 +0000

    added images game.js

--- js/game.js
+++ js/game.js
@@ -5,6 +5,7 @@ function init() {
     canvas = document.getElementById('canvas');
     canvas.width = 720;
     canvas.height = 480;
+    character. src = '../img/2. Secuencias_Personaje-Pepe-corrección/2. Secuencia_caminata/W-21.png';
 //    const ctx = canvas.getContext("2d");
 
 }

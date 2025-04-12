0d4f6784231383a51fe0f1a9f5ade65248d0bf8f
LiaSab24 <164032382+LiaSab24@users.noreply.github.com>
2025-04-12 12:16:38 +0000

    added coment

--- classes/character.class.js
+++ classes/character.class.js
@@ -5,7 +5,7 @@ class  Character extends MovableObject {
 
     constructor() {
         super();
-        this.loadImage('img/pepe/caminata/1.png');
+        this.loadImage('img/pepe/caminata/1.png');  // oder super().loadImage('img/pepe/caminata/1.png'); super() bezieht sich auf die übergeordnete class
       }
     
       jump() {

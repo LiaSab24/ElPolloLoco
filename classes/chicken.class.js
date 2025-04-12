f872e1833589800e6a52106945298deea0dc2906
LiaSab24 <164032382+LiaSab24@users.noreply.github.com>
2025-04-12 12:17:42 +0000

    added constructor

--- classes/chicken.class.js
+++ classes/chicken.class.js
@@ -2,7 +2,11 @@ class  Chicken extends MovableObject {
     width;
     height;
 
-
+    constructor() {
+        super();
+        this.loadImage('img/pepe/caminata/1.png');  // oder super().loadImage('img/pepe/caminata/1.png'); super() bezieht sich auf die übergeordnete class
+      }
+      
     run () { 
     }
 }

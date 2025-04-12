42813ff99a83a6e97fef15e45d938c39d0bf862f
LiaSab24 <164032382+LiaSab24@users.noreply.github.com>
2025-04-12 09:46:36 +0000

    changed width canvas

--- js/game.js
+++ js/game.js
@@ -4,7 +4,7 @@ let character = new MovableObject();              //Image();
 
 function init() {
     canvas = document.getElementById('canvas');
-    canvas.width = 720;
+    canvas.width = 800;
     canvas.height = 480;
     ctx = canvas.getContext('2d');
   

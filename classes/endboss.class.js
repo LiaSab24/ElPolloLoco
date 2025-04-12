4704fbf5a9507a17f3609cc6fc91874d970bb1ac
LiaSab24 <164032382+LiaSab24@users.noreply.github.com>
2025-04-12 12:36:25 +0000

    added pirat

--- classes/endboss.class.js
+++ classes/endboss.class.js
@@ -1,10 +1,12 @@
 class  EndBoss extends MovableObject {
+    x = 700;
+    y = 250;
     width = 50;
     height = 150;
 
     constructor() {
         super();
-        this.loadImage('img/pirat/WALK_000.png');  // oder super().loadImage('img/pepe/caminata/1.png'); super() bezieht sich auf die übergeordnete class
+        this.loadImage('img/pirat/WALK_000.png');  // oder super().loadImage('img/pirat/WALK_000.png'); super() bezieht sich auf die übergeordnete class
       }
       
     run () { 

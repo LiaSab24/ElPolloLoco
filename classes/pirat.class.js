fe2e9edb5a3f8ee6464f9e0a412c545c3230ca4e
LiaSab24 <164032382+LiaSab24@users.noreply.github.com>
2025-04-13 06:28:46 +0000

    added links in html, update pirat.class

--- classes/pirat.class.js
+++ classes/pirat.class.js
@@ -1,13 +1,14 @@
-class Pirat extends MovableObject{
-    x = 700;
-    width = 50;
-    height = 150;
+class  Pirat2 extends MovableObject {
+  x = 700;
+  width = 50;
+  height = 150;
 
-    constructor() {
-        super();
-        this.loadImage('img/pirat/WALK_000.png');  // oder super().loadImage('img/pirat/WALK_000.png'); super() bezieht sich auf die übergeordnete class
-      }
-      
-    run () { 
+  constructor() {
+      super();
+      this.loadImage('img/pirat/2_WALK_000.png');  // oder super().loadImage('img/pirat2/2_WALK_000.png'); super() bezieht sich auf die übergeordnete class
     }
+    
+  run () { 
+      console.log('Running!');
+  }
 }

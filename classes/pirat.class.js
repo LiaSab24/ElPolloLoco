0abf7b606a80802f1881a0dcfe6d555778eec796
LiaSab24 <164032382+LiaSab24@users.noreply.github.com>
2025-04-13 06:30:49 +0000

    update img path

--- classes/pirat.class.js
+++ classes/pirat.class.js
@@ -5,7 +5,7 @@ class  Pirat extends MovableObject {
 
   constructor() {
       super();
-      this.loadImage('img/pirat/2_WALK_000.png');  // oder super().loadImage('img/pirat2/2_WALK_000.png'); super() bezieht sich auf die übergeordnete class
+      this.loadImage('img/pirat/WALK_000.png');  // oder super().loadImage('img/pirat2/2_WALK_000.png'); super() bezieht sich auf die übergeordnete class
     }
     
   run () { 
